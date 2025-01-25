63043c337881b39f77fe95226414233df852fbc9
Amrith Gandham <amrithgandham@gmail.com>
2025-01-24 18:10:43 -0800

    Create backend and frontend

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+# from flask_cors import CORS
+
+app = Flask(__name__)
+# CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/api', methods=['GET'])
+def hello():
+    return {'message': 'Hello, World!'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
