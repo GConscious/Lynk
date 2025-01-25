aef57804f979a7c6f75baa3da25e5e946df8db13
Asad-Jaffery <asadmjaffery@gmail.com>
2025-01-25 15:22:01 -0800

    create profile page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BusinessSignup from "./components/BusinessSignup.jsx";
 import Events from "./components/Events.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import BusinessLogin from "./components/Login.jsx";
+import BusinessLogin from "./components/BusinessLogin.jsx";
 
 function App() {
   return (
